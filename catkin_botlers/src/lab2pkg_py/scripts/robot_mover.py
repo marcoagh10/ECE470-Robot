7c63847e098afd29912e50d5f06f7d2271d7a4c1
weiangw2 <weiangw2@illinois.edu>
2022-10-22 15:09:14 -0500

    Setup intial code for moving from point a to b

---
+++ catkin_botlers/src/lab2pkg_py/scripts/robot_mover.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+from cv2 import erode
+import rospy
+from tf.transformations import euler_from_quaternion
+from nav_msgs.msg import Odometry
+import numpy as np
+from geometry_msgs.msg import Twist
+from gazebo_msgs.msg import ModelStates
+
+
+class RobotMover:
+    def __init__(self) -> None:
+        self.position_sub = rospy.Subscriber("/gazebo/model_states", ModelStates, self.position_callback)
+        self.vel_pub = rospy.Publisher("/jackal/jackal_velocity_controller/cmd_vel", Twist, queue_size=1)
+
+        self.cmd_vel = Twist()
+
+        self.robot_data = [0,0,0,0]
+
+        self.received_first = False
+
+        self.wait_for_pose()
+
+        print("recieved first")
+
+    def wait_for_pose(self):
+        rate = rospy.Rate(60)
+
+        while not rospy.is_shutdown() and not self.received_first:
+            rate.sleep()
+
+    def set_speeds(self, lin=0, ang=0):
+        self.cmd_vel.linear.x = lin
+        self.cmd_vel.angular.z = ang
+
+
+    def position_callback(self, msg:ModelStates):
+        id = -1
+        
+        for i, name in enumerate(msg.name):
+            if name == "jackal":
+                id = i
+        if id == -1:
+            return
+
+        pose = msg.pose[id]
+
+        p_x, p_y, p_z = pose.position.x,pose.position.y,pose.position.z
+        o_x, o_y, o_z, o_w = pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w
+
+        roll, pitch, yaw = euler_from_quaternion([o_x, o_y, o_z, o_w])
+
+        # print(np.degrees(roll), np.degrees(pitch), np.degrees(yaw))
+
+        self.robot_data = [p_x,p_y, p_z, yaw]
+
+        self.received_first = True
+
+    def move_to(self, end_pose):
+        x_t, y_t, z_t, yaw_t = end_pose
+
+        target_angle = np.arctan2(y_t - self.robot_data[1], x_t - self.robot_data[0])
+
+
+    def move_rotate_angle(self, target_pose, target_angle):
+        rate = rospy.Rate(60)
+
+        error_ang = np.arctan2(target_pose[1] - self.robot_data[1], target_pose[0] - self.robot_data[0]) - self.robot_data[3]
+        error_dist = np.sqrt((self.robot_data[0] - target_pose[0])**2 +  (self.robot_data[1] - target_pose[1])**2 ) 
+
+        if abs(error_ang) > np.pi:
+            error_ang = np.sign(error_ang) * abs(error_ang) % np.pi
+
+        while abs(error_ang) > np.radians(2) or abs(error_dist) > .1:
+            error_ang = np.arctan2(target_pose[1] - self.robot_data[1], target_pose[0] - self.robot_data[0])  - self.robot_data[3] 
+            
+            if abs(error_ang) > np.pi:
+                error_ang = np.sign(error_ang) * abs(error_ang) % np.pi
+
+
+            error_dist = np.sqrt((self.robot_data[0] - target_pose[0])**2 +  (self.robot_data[1] - target_pose[1])**2 ) 
+
+            vel_control = min(error_dist * .25, 1)
+            ang_control = error_ang * 1
+
+            print(error_ang, error_dist)
+
+            self.set_speeds(lin=vel_control,ang=ang_control)  
+
+            self.pub()
+            rate.sleep()
+
+        error_ang = target_angle -  self.robot_data[3]
+
+        if abs(error_ang) > np.pi:
+            error_ang = np.sign(error_ang) * abs(error_ang) % np.pi
+
+        self.set_speeds()
+
+        while abs(error_ang) > np.radians(2):
+            error_ang = target_angle -  self.robot_data[3]
+
+            if abs(error_ang) > np.pi:
+                error_ang = np.sign(error_ang) * abs(error_ang) % np.pi
+
+            ang_control = error_ang * 1
+
+            self.set_speeds(ang=ang_control)  
+
+            self.pub()
+            rate.sleep()
+    
+    def pub(self):
+        self.vel_pub.publish(self.cmd_vel)
+
+
+    def run(self):
+        rate = rospy.Rate(60)
+
+        self.move_rotate_angle([0, 0], np.radians(0))
+
+        while not rospy.is_shutdown():
+            rate.sleep()
+
+
+if __name__ == "__main__":
+    rospy.init_node("robot_mover")
+
+    p = RobotMover()
+
+    p.run()
